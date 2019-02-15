95ce66fbfc7746b2779217a375f6429a5f77d906
mark <mark>
2019-02-14 20:28:15 -0500

    WIP Need to figure out good format

---
+++ format-message.js
@@ -0,0 +1,7 @@
+const { RichEmbed } = require('discord.js');
+
+const rssEntryToRichText = (entry) => {
+  new RichEmbed()
+    .setTitle(entry.title)
+    .setAuthor
+}
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
-const { Client, RichEmbed } = require('discord.js');
+const { Client } = require('discord.js');
 const Parser = require('rss-parser');
 const schedule = require('node-schedule');
+const { parse } = require('node-html-parser');
 
 const bot_token = process.env.BOT_TOKEN;
 const channel_id = process.env.CHANNEL_ID;
@@ -14,12 +15,16 @@ let last_updated = new Date().toISOString();
 const fetch_and_update_feed = async () => {
   let feed = await rss_parser.parseURL('https://www.reddit.com/r/mechmarket/new/.rss');
   console.log(last_updated, Date.now(), feed.title);
+console.log(feed.items[0])
   const entries = feed.items
     .filter((entry) => entry.isoDate > last_updated)
     .reverse();
 
   console.log('Entries: ', entries.length);
-  console.log(entries)
+  console.log(entries);
+
+  const root = parse(feed.items[2].content)
+  // console.log(root.firstChild.querySelectorAll('a'))
 
   for (entry of entries) {
     const message = entry.title + ' ' + entry.link
@@ -27,20 +32,20 @@ const fetch_and_update_feed = async () => {
     last_updated = entry.isoDate;
   }
 };
+fetch_and_update_feed();
+// discord_client.on('ready', () => {
+//   schedule.scheduleJob('*/30 * * * * *', () => {
+//     fetch_and_update_feed()
+//   });
+// });
 
-discord_client.on('ready', () => {
-  schedule.scheduleJob('*/30 * * * * *', () => {
-    fetch_and_update_feed()
-  });
-});
+// discord_client.on('message', message => {
+//   if ('!update' == message.content) {
+//     fetch_and_update_feed();
+//   }
+//   else if ('!test' == message.content) {
+//     discord_client.channels.get(channel_id).send("<@" + message.author.id + ">");
+//   }
+// });
 
-discord_client.on('message', message => {
-  if ('!update' == message.content) {
-    fetch_and_update_feed();
-  }
-  else if ('!test' == message.content) {
-    discord_client.channels.get(channel_id).send("<@" + message.author.id + ">");
-  }
-});
-
-discord_client.login(bot_token);
+// discord_client.login(bot_token);
